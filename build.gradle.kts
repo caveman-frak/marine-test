04f922131376353d4f7585a312a420c9bd66dedf
Caveman Frak <tim.pickard@bluegecko.co.uk>
2023-03-15 07:19:26 +0000

    first application / integration test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,7 @@
 plugins {
-    id("java")
-    id("io.freefair.lombok") version "8.0.0-rc2"
-    id("io.spring.dependency-management") version "1.1.0"
-}
-
-repositories {
-    mavenCentral()
+    id("marine.library-conventions")
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-test:3.0.4")
-}
-
-testing {
-    suites {
-        val test by getting(JvmTestSuite::class) {
-            useJUnitJupiter()
-        }
-    }
+    implementation("org.springframework.boot:spring-boot-starter-test")
 }
